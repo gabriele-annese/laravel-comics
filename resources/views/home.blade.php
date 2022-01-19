c57e9705e934a051272b3f6c2505e9e7369708b1
gabriele-annese <gannese06@gmail.com>
2022-01-19 17:41:48 +0100

    add main content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,16 @@
 @section('main-content')
     <section class="homepage">
         <div class="main-ctn">
-            
+            <div class="cards">
+                @foreach ($cards as $card )
+                    <div class="card">
+                        <div class="img-ctn">
+                            <img src="{{ $card['thumb'] }}" alt="{{ $card['title'] }}">
+                        </div>
+                        <h4>{{ $card['series'] }}</h4>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </section>
 @endsection
